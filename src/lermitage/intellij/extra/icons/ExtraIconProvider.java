90744c70481eb665a72aa295e58154b1bb32e068
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2018-08-25 16:13:42 +0200

    reworked Maven wrapper and Gradle wrapper icons

--- src/lermitage/intellij/extra/icons/ExtraIconProvider.java
+++ src/lermitage/intellij/extra/icons/ExtraIconProvider.java
@@ -15,7 +15,11 @@ public class ExtraIconProvider extends IconProvider {
     private static final String JENKINS_ICON = "/icons/jenkins.png";
     private static final String GITLAB_ICON = "/icons/gitlab.png";
     private static final String BASH_ICON = "/icons/bash.png";
+    private static final String BASH_GRADLEW_ICON = "/icons/bash-gradlew.png";
+    private static final String BASH_MVNW_ICON = "/icons/bash-mvnw.png";
     private static final String CMD_ICON = "/icons/cmd.png";
+    private static final String CMD_GRADLEW_ICON = "/icons/cmd-gradlew.png";
+    private static final String CMD_MVNW_ICON = "/icons/cmd-mvnw.png";
     
     public Icon getIcon(@NotNull PsiElement psiElement, int flags) {
         PsiFile containingFile = psiElement.getContainingFile();
@@ -30,8 +34,16 @@ public class ExtraIconProvider extends IconProvider {
                 return IconLoader.getIcon(JENKINS_ICON);
             } else if (name.equals(".gitlab-ci.yml")) {
                 return IconLoader.getIcon(GITLAB_ICON);
-            } else if (name.endsWith(".sh") || name.equals("gradlew") || name.equals("mvnw")) {
+            } else if (name.equals("gradlew")) {
+                return IconLoader.getIcon(BASH_GRADLEW_ICON);
+            } else if (name.equals("mvnw")) {
+                return IconLoader.getIcon(BASH_MVNW_ICON);
+            } else if (name.endsWith(".sh")) {
                 return IconLoader.getIcon(BASH_ICON);
+            } else if (name.equals("gradlew.cmd") || name.equals("gradlew.bat")) {
+                return IconLoader.getIcon(CMD_GRADLEW_ICON);
+            } else if (name.equals("mvnw.cmd") || name.equals("mvnw.bat")) {
+                return IconLoader.getIcon(CMD_MVNW_ICON);
             } else if (name.endsWith(".cmd") || name.endsWith(".bat") || name.endsWith(".ps1")) {
                 return IconLoader.getIcon(CMD_ICON);
             }
