680629d811bf42c913386d17f69735ad2143c9df
Edoardo Luppi <lp.edoardo@gmail.com>
2018-12-01 17:12:55 +0100

    Add and manage icons for Angular2+ modules
    Icons are used only on files which are inside an Angular2+ context,
    and only if the AngularJS plugin is enabled.

---
+++ src/lermitage/intellij/extra/icons/Angular2IconProvider.java
@@ -0,0 +1,49 @@
+package lermitage.intellij.extra.icons;
+
+import com.intellij.ide.IconProvider;
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import org.angular2.lang.Angular2LangUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
+
+/**
+ * Icon provider specific for Angular2+ modules.
+ *
+ * @author Edoardo Luppi
+ */
+class Angular2IconProvider extends IconProvider {
+	private static final Collection<Model> MODELS =
+			Arrays.asList(
+					new Model("/icons/angular-module.svg").end(".module.ts"),
+					new Model("/icons/angular-component.svg").end(".component.ts"),
+					new Model("/icons/angular-service.svg").end(".service.ts"),
+					new Model("/icons/angular-pipe.svg").end(".pipe.ts"),
+					new Model("/icons/angular-directive.svg").end(".directive.ts"),
+					new Model("/icons/angular-guard.svg").end(".guard.ts"));
+
+	@Nullable
+	@Override
+	public Icon getIcon(@NotNull final PsiElement psiElement, final int flags) {
+		final Optional<PsiFile> optFile = Optional.ofNullable(psiElement.getContainingFile());
+		final PsiFile file;
+
+		if (optFile.isPresent() && Angular2LangUtil.isAngular2Context(file = optFile.get())) {
+			final String fileName = file.getName().toLowerCase();
+
+			for (final Model model : MODELS) {
+				if (model.check(fileName)) {
+					return IconLoader.getIcon(model.getIcon());
+				}
+			}
+		}
+
+		return null;
+	}
+}
