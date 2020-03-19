bc2d8aedb1b6b74d4c2a5841b4015f7ed3b0bbfa
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2020-03-19 16:12:31 +0100

    test plugin in latest IJ EAP

--- do.cmd
+++ do.cmd
@@ -5,6 +5,7 @@ if [%1] == [help] (
   echo  fixgit:  fix permission flag on git index for required files
   echo  run :    run plugin in IntelliJ Ultimate 2018.3
   echo  run2019: run plugin in IntelliJ Ultimate 2019.3
+  echo  runeap:  run plugin in latest IntelliJ Ultimate EAP Snapshot
   echo  release: package plugin
   echo  test:    run unit tests
   echo  cv:      check dependencies and Gradle updates
@@ -22,6 +23,9 @@ if [%1] == [run] (
 )
 if [%1] == [run2019] (
   gradlew buildPlugin runIde --warning-mode all -PideaVersion=IU-2019.3
+)
+if [%1] == [runeap] (
+  gradlew buildPlugin runIde --warning-mode all -PideaVersion=IU-LATEST-EAP-SNAPSHOT
 )
 if [%1] == [release] (
   gradlew clean buildPlugin test verifyPlugin --warning-mode all
