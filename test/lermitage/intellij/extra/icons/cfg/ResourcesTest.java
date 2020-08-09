2ae8f0dce6bae8c5fc529783bd7d24621bf7e3bf
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2020-08-10 01:56:49 +0200

    improve tests

---
+++ test/lermitage/intellij/extra/icons/cfg/ResourcesTest.java
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: MIT
+
+package lermitage.intellij.extra.icons.cfg;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ResourcesTest {
+
+    static List<File> icons;
+    static List<File> svgIcons;
+    static List<File> pngIcons;
+
+    @BeforeAll
+    static void setUp() {
+        File iconsFolder = new File("resources/icons/");
+        icons = Arrays.asList(iconsFolder.listFiles((dir, name) -> name.endsWith(".png") || name.endsWith(".svg")));
+        pngIcons = icons.stream().filter(file -> file.getName().endsWith(".png")).collect(Collectors.toList());
+        svgIcons = icons.stream().filter(file -> file.getName().endsWith(".svg")).collect(Collectors.toList());
+        assertTrue(svgIcons.size() > 0);
+        assertTrue(pngIcons.size() > 0);
+        icons.forEach(file -> assertTrue(file.getName().endsWith(".png") || file.getName().endsWith(".svg")));
+    }
+
+    @Test
+    public void icons_should_not_exist_as_svg_and_png() {
+        Set<String> pngIconNames = pngIcons.stream()
+            .map(file -> file.getName().replace(".png", ""))
+            .collect(Collectors.toSet());
+
+        for (File svgIcon : svgIcons) {
+            String svgIconName = svgIcon.getName().replace(".svg", "");
+            assertFalse(pngIconNames.contains(svgIconName), svgIconName);
+            assertFalse(pngIconNames.contains(svgIconName + "_dark"), svgIconName);
+            assertFalse(pngIconNames.contains(svgIconName + "@2x"), svgIconName);
+            assertFalse(pngIconNames.contains(svgIconName + "@2x_dark"), svgIconName);
+        }
+    }
+
+    @Test
+    public void png_icons_should_have_2x_definition() {
+        Set<String> pngIconNames = pngIcons.stream()
+            .map(file -> file.getName().replace(".png", ""))
+            .collect(Collectors.toSet());
+        List<String> errors = new ArrayList<>();
+
+        for (String icon : pngIconNames) {
+            if (icon.endsWith("_dark")) {
+                if (icon.endsWith("@2x_dark")) {
+                    if (!pngIconNames.contains(icon.replace("@2x_dark", "_dark"))) {
+                        errors.add(icon + " should be coupled with _dark");
+                    }
+                } else {
+                    if (!pngIconNames.contains(icon.replace("_dark", "@2x_dark"))) {
+                        errors.add(icon + " should be coupled with @2x_dark");
+                    }
+                }
+            } else {
+                if (icon.endsWith("@2x")) {
+                    if (!pngIconNames.contains(icon.substring(0, icon.length() - "@2x".length()))) {
+                        errors.add(icon + " should be coupled with non-@2x");
+                    }
+                } else {
+                    if (!pngIconNames.contains(icon + "@2x")) {
+                        errors.add(icon + " should be coupled with @2x");
+                    }
+                }
+            }
+        }
+        if (!errors.isEmpty()) {
+            fail("some PNG icons don't have 2x definition : " + errors);
+        }
+    }
+
+    @Test
+    public void dark_icon_should_be_coupled_with_light_icon() {
+        File iconsFolder = new File("resources/icons/");
+        Set<String> iconNames = icons.stream()
+            .map(File::getName)
+            .collect(Collectors.toSet());
+        List<String> errors = new ArrayList<>();
+
+        for (String icon : iconNames) {
+            if (icon.contains("_dark") && !iconNames.contains(icon.replace("_dark", ""))) {
+                errors.add(icon);
+            }
+        }
+        if (!errors.isEmpty()) {
+            fail("some dark icons are not coupled with non-dark icons: " + errors);
+        }
+    }
+
+    @Test
+    public void svg_icons_should_be_16x16() {
+        File iconsFolder = new File("resources/icons/");
+        List<String> errors = new ArrayList<>();
+
+        svgIcons.forEach(file -> {
+            try {
+                String fileContent = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+                if (!fileContent.contains("width=\"16\"") || !fileContent.contains("height=\"16\"")) {
+                    errors.add(file.getName());
+                }
+            } catch (IOException e) {
+                fail(e);
+            }
+        });
+        if (!errors.isEmpty()) {
+            fail("some SVG icons are not 16x16: " + errors);
+        }
+    }
+}
--- test/lermitage/intellij/extra/icons/cfg/SettingsServiceTest.java
+++ test/lermitage/intellij/extra/icons/cfg/SettingsServiceTest.java
@@ -10,11 +10,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class SettingsServiceTest {
@@ -24,6 +20,7 @@ public class SettingsServiceTest {
         List<Model> allRegisteredModels = SettingsService.getAllRegisteredModels();
         Set<String> ids = new HashSet<>();
         Set<String> duplicatedIds = new HashSet<>();
+
         for (Model model : allRegisteredModels) {
             String id = model.getId();
             if (ids.contains(id)) {
@@ -40,73 +37,14 @@ public class SettingsServiceTest {
     public void registered_models_should_have_existing_icon_file() {
         List<Model> allRegisteredModels = SettingsService.getAllRegisteredModels();
         List<String> errors = new ArrayList<>();
+
         allRegisteredModels.forEach(model -> {
             if (!new File("resources", model.getIcon()).exists()) {
                 errors.add(model.getId() + ": " + model.getIcon());
             }
         });
         if (!errors.isEmpty()) {
-            fail("some icons are missing in resources folder:" + errors);
-        }
-    }
-
-    @Test
-    public void icons_should_not_exist_as_svg_and_png() {
-        File iconsFolder = new File("resources/icons/");
-        File[] svgIcons = iconsFolder.listFiles((dir, name) -> name.endsWith(".svg"));
-        File[] pngIcons = iconsFolder.listFiles((dir, name) -> name.endsWith(".png"));
-        Set<String> pngIconNames = Stream.of(pngIcons)
-            .map(file -> file.getName().replace(".png", ""))
-            .collect(Collectors.toSet());
-        assertTrue(svgIcons.length > 0);
-        assertTrue(pngIcons.length > 0);
-
-        for (File svgIcon : svgIcons) {
-            String svgIconName = svgIcon.getName().replace(".svg", "");
-            assertFalse(pngIconNames.contains(svgIconName), svgIconName);
-            assertFalse(pngIconNames.contains(svgIconName + "_dark"), svgIconName);
-            assertFalse(pngIconNames.contains(svgIconName + "@2x"), svgIconName);
-            assertFalse(pngIconNames.contains(svgIconName + "@2x_dark"), svgIconName);
-        }
-    }
-
-    @Test
-    public void png_icons_should_have_2x_definition() {
-        File iconsFolder = new File("resources/icons/");
-        File[] pngIcons = iconsFolder.listFiles((dir, name) -> name.endsWith(".png"));
-        Set<String> pngIconNames = Stream.of(pngIcons)
-            .map(file -> file.getName().replace(".png", ""))
-            .collect(Collectors.toSet());
-
-        for (String icon : pngIconNames) {
-            if (icon.endsWith("_dark")) {
-                if (icon.endsWith("@2x_dark")) {
-                    assertTrue(pngIconNames.contains(icon.replace("@2x_dark", "_dark")), icon);
-                } else {
-                    assertTrue(pngIconNames.contains(icon.replace("_dark", "@2x_dark")), icon);
-                }
-            } else {
-                if (icon.endsWith("@2x")) {
-                    assertTrue(pngIconNames.contains(icon.substring(0, icon.length() - "@2x".length())), icon);
-                } else {
-                    assertTrue(pngIconNames.contains(icon + "@2x"), icon);
-                }
-            }
-        }
-    }
-
-    @Test
-    public void dark_icon_should_be_coupled_with_light_icon() {
-        File iconsFolder = new File("resources/icons/");
-        File[] icons = iconsFolder.listFiles((dir, name) -> name.endsWith(".png") || name.endsWith(".svg"));
-        Set<String> iconNames = Stream.of(icons)
-            .map(File::getName)
-            .collect(Collectors.toSet());
-
-        for (String icon : iconNames) {
-            if (icon.contains("_dark")) {
-                assertTrue(iconNames.contains(icon.replace("_dark", "")), icon);
-            }
+            fail("some icons are missing in resources folder: " + errors);
         }
     }
 }
