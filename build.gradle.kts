fc58b7d544c6e998e6f1a2cc21f0101050372947
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2020-11-06 01:44:53 +0100

    upgrade gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 plugins {
     id("java")
     id("idea")
-    id("org.jetbrains.intellij") version "0.6.1" // https://github.com/JetBrains/gradle-intellij-plugin
+    id("org.jetbrains.intellij") version "0.6.2" // https://github.com/JetBrains/gradle-intellij-plugin
     id("com.github.ben-manes.versions") version "0.34.0" // https://github.com/ben-manes/gradle-versions-plugin
 }
 
