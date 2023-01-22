04e4abe0ce9c429eaa21a3e44190201249548a1c
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2023-01-21 20:18:18 -0500

    upgrade gradle-versions-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ fun properties(key: String) = project.findProperty(key).toString()
 plugins {
     id("java")
     id("jacoco")
-    id("org.jetbrains.intellij") version "1.11.0" // https://github.com/JetBrains/gradle-intellij-plugin
+    id("org.jetbrains.intellij") version "1.12.0" // https://github.com/JetBrains/gradle-intellij-plugin
     id("org.jetbrains.changelog") version "2.0.0" // https://github.com/JetBrains/gradle-changelog-plugin
     id("com.github.ben-manes.versions") version "0.44.0" // https://github.com/ben-manes/gradle-versions-plugin
     id("com.adarshr.test-logger") version "3.2.0" // https://github.com/radarsh/gradle-test-logger-plugin
@@ -57,8 +57,8 @@ repositories {
 }
 
 val twelvemonkeysVersion = "3.9.4"
-val junitVersion = "5.9.1"
-val junitPlatformLauncher = "1.9.1"
+val junitVersion = "5.9.2"
+val junitPlatformLauncher = "1.9.2"
 
 dependencies {
     implementation("com.twelvemonkeys.imageio:imageio-core:$twelvemonkeysVersion") // https://github.com/haraldk/TwelveMonkeys/releases
