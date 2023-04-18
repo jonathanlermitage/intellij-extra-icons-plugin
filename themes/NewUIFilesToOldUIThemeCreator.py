590ad0b31cc07d7cb1e8706f3e8c6af536c2d33f
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2023-04-17 21:24:03 -0400

    improve reporting

--- themes/NewUIFilesToOldUIThemeCreator.py
+++ themes/NewUIFilesToOldUIThemeCreator.py
@@ -1,9 +1,15 @@
 #  SPDX-License-Identifier: MIT
 import base64
 import glob
+import hashlib
 import os
 import sys
 from os.path import exists
+
+
+OK = " 🟢 "
+NEW = " 🔵 "
+ERR = " 🔴 "
 
 
 def convert_icons_to_b64(icons: dict) -> dict:
@@ -13,7 +19,7 @@ def convert_icons_to_b64(icons: dict) -> dict:
             file_str = f.read().encode('utf-8')
             encoded = base64.b64encode(file_str)
             converted_icons[icon] = encoded.decode("utf-8")
-    print(f"✅  Converted {len(icons)} icons to Base64")
+    print(f"{OK}Converted {len(icons)} icons to Base64")
     return converted_icons  # icon paths with base64 pictures
 
 
@@ -29,6 +35,16 @@ def icon_pack_ij(items: list[str], version: str) -> str:
         .replace(": ", ":")
 
 
+def md5_sum(filename: str) -> str:
+    if exists:
+        file_hash = hashlib.md5()
+        with open(filename, "rb") as f:
+            for chunk in iter(lambda: f.read(128 * file_hash.block_size), b""):
+                file_hash.update(chunk)
+        return file_hash.hexdigest()
+    return ""
+
+
 def icon_pack_ij_item(icon_path, icon_b64) -> str:
     template = """{"ideIcon": "{icon_path}", "icon": "{icon_b64}", "description": "{icon_path}", "iconPack": "", "modelType": "ICON", "iconType": "SVG", "enabled": true, "conditions": [{"start": false, "eq": false, "mayEnd": false, "end": false, "noDot": false, "checkParent": false, "hasRegex": false, "enabled": true, "checkFacets": false, "hasIconEnabler": false, "names": [], "parentNames": [], "extensions": [], "facets": []}]}"""  # NOPEP8
     return template.replace("{icon_path}", icon_path).replace("{icon_b64}", icon_b64)
@@ -39,9 +55,9 @@ if __name__ == '__main__':
     icon_pack_version = sys.argv[2]
 
     if not ij_sources_folder_input:
-        raise ValueError("❌  IntelliJ sources folder required")
+        raise ValueError(f"{ERR}IntelliJ sources folder required")
     if not exists(ij_sources_folder_input):
-        raise ValueError(f"❌  IntelliJ sources folder '{ij_sources_folder_input}' not found")
+        raise ValueError(f"{ERR}IntelliJ sources folder '{ij_sources_folder_input}' not found")
 
     if not icon_pack_version:
         icon_pack_version = "1"
@@ -61,7 +77,7 @@ if __name__ == '__main__':
 
     sub_folders_whitelist = ["fileTypes", "nodes"]
 
-    print(f"✅  Loading all IJ SVG icons (old and new UI) from {ij_sources_folder_input}/platform/icons/")
+    print(f"{OK}Loading all IJ SVG icons (old and new UI) from {ij_sources_folder_input}/platform/icons/")
     icon_pack = {}
     for file in glob.glob(f"{ij_sources_folder_input}/platform/icons/src/expui/**/*.svg"):
         file = file.replace("\\", "/")
@@ -78,10 +94,10 @@ if __name__ == '__main__':
                 alt_file = file.replace(icon_substitution, path_substitutions[icon_substitution])
                 break
         if exists(file.replace("/expui/", "/")):
-            icon_pack[file.replace(f"{ij_sources_folder_input}/platform/icons/src/expui/", "/")] = file.replace("/expui/", "/")
+            icon_pack[file.replace(f"{ij_sources_folder_input}/platform/icons/src/expui/", "/")] = file.replace("/expui/", "/")  # NOPEP8
         elif alt_file and exists(alt_file.replace("/expui/", "/")):
-            icon_pack[file.replace(f"{ij_sources_folder_input}/platform/icons/src/expui/", "/")] = alt_file.replace("/expui/", "/")
-    print(f"✅  Found {len(icon_pack)} valid icons for Icon Pack")
+            icon_pack[file.replace(f"{ij_sources_folder_input}/platform/icons/src/expui/", "/")] = alt_file.replace("/expui/", "/")  # NOPEP8
+    print(f"{OK}Found {len(icon_pack)} valid icons for Icon Pack")
 
     icon_pack = convert_icons_to_b64(icon_pack)
 
@@ -96,9 +112,14 @@ if __name__ == '__main__':
 
     json_icon_pack = icon_pack_ij(icon_pack_items, icon_pack_version)
 
+    old_md5 = None
     if exists("NewUIFilesToOldUITheme.json"):
+        old_md5 = md5_sum("NewUIFilesToOldUITheme.json")
         os.remove("NewUIFilesToOldUITheme.json")
-        print(f"✅  Removed existing NewUIFilesToOldUITheme.json Icon Pack")
+        print(f"{OK}Removed existing NewUIFilesToOldUITheme.json Icon Pack")
     with open("NewUIFilesToOldUITheme.json", "w", newline="\n") as json_icon_pack_file:
         json_icon_pack_file.write(json_icon_pack)
-    print(f"✅  (★‿★) Created NewUIFilesToOldUITheme.json Icon Pack (★‿★)")
+    print(f"{OK}つ ◕_◕ ༽つ Created NewUIFilesToOldUITheme.json Icon Pack")
+    new_md5 = md5_sum("NewUIFilesToOldUITheme.json")
+    if new_md5 != old_md5:
+        print(f"{NEW}つ ◕_◕ ༽つ NewUIFilesToOldUITheme.json is new!")
